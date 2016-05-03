10e7e1f1c01165b116be32912fc68cfd32efd227
zhoushengtao <zhoushengtao@chengyiwm.com>
2016-05-03 22:33:47 +0800

    增加了达标与未达标的输出

--- src/com/huangyiting/makeexercise/FillterHabit.java
+++ src/com/huangyiting/makeexercise/FillterHabit.java
@@ -7,17 +7,35 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FillterHabit {
-	private static final String FILE_NAME = "2016-04-24.txt";
-	private static final String FILE_OUT_NAME = FILE_NAME + ".out.txt";
+	private static String FILE_NAME = "2016-04-24.txt";
+	private static String FILE_OUT_NAME = FILE_NAME + ".out.txt";
+	private static int REACH_COUNT = 3;
 	
 	
 
 
 	public static void main(String[] args) {
 		ArrayList<HabitBean> userContents = new ArrayList<HabitBean>();
+		Map<String, ArrayList<HabitBean>> notReachuserContents = new HashMap<>();
+		
+		if (args != null && args.length>=1 && args[0] != null) {
+			FILE_NAME = args[0];
+		} else {
+			wirtehelp();
+			return;
+		}
+		
+//		if (args != null && args.length>=2 && args[0] != null) {
+//			try{
+//				REACH_COUNT = Integer.valueOf(args[1]);
+//			} catch (Exception e) {
+//				
+//			}
+//		}
 		
 		try {
 	        FileInputStream fis = new FileInputStream(FILE_NAME); 
@@ -53,12 +71,42 @@ public class FillterHabit {
 			}
 			
 			
-			Collections.sort(userContents , new HabitComparator());
-			for (HabitBean bean : userContents) {
-				bufferedWriter.write(bean.userName + "\t");
-				bufferedWriter.write(bean.time + "\t");
-				bufferedWriter.write(bean.content + "\n");
+			// 转化list 2 map
+			for(HabitBean bean: userContents) {
+				ArrayList<HabitBean> array = notReachuserContents.get(bean.userName);
+				if(array == null) {
+					array = new ArrayList<>();
+				}
+				array.add(bean);
+				notReachuserContents.put(bean.userName, array);
 			}
+			
+			bufferedWriter.write("已达标\n");
+			for (String username : notReachuserContents.keySet()) {
+				if(notReachuserContents.get(username).size() >= REACH_COUNT) {
+					for (HabitBean bean: notReachuserContents.get(username)) {
+						writeInfoOut(bufferedWriter, bean);
+					}
+				}
+			}
+			
+			bufferedWriter.write("未达标\n");
+			for (String username : notReachuserContents.keySet()) {
+				if(notReachuserContents.get(username).size() < REACH_COUNT) {
+					for (HabitBean bean: notReachuserContents.get(username)) {
+						writeInfoOut(bufferedWriter, bean);
+					}
+				}
+			}
+			
+//			for (HabitBean bean : userContents) {
+//				bufferedWriter.write(bean.userName + "\t");
+//				bufferedWriter.write(bean.time + "\t");
+//				bufferedWriter.write(bean.content + "\n");
+//			}
+
+			System.out.println("写出成功！！！！！！1\n");
+			System.out.println("存储在："+ FILE_OUT_NAME +"\n");
 
 			bufferedWriter.flush();
 			bufferedReader.close();
@@ -66,6 +114,26 @@ public class FillterHabit {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	private static void wirtehelp() {
+		System.out.println("+===========================================================+\n");
+		System.out.println("|=======================小点健康统计程序 v1.0====================|\n");
+		System.out.println("+===========================================================+\n");
+		System.out.println("使用帮助：+\n");
+		System.out.println("      \n");
+		System.out.println("option: 输入文件路径 \n");
+		System.out.println("demo: xiaodian.jar d:/xiaodian.txt \n");
+		System.out.println("\n\n\n");
+	}
+	
+	//格式化输出
+	private static void writeInfoOut(BufferedWriter bufferedWriter, HabitBean bean) throws Exception{
+
+		bufferedWriter.write(bean.userName + "\t");
+		bufferedWriter.write(bean.time + "\t");
+		bufferedWriter.write(bean.content + "\n");
 	}
 
 }
